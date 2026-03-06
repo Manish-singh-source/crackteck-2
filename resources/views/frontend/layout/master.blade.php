53b2ac064eebc7868f193f7229c743f02ea1938e
saurabh <support@technofra.com>
2026-03-06 15:34:17 +0530

    fixed breadcrumb url

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="utf-8">
     <title>Crakteck </title>
-    <meta name="author" content="Technofra.com">
+    <meta name="author" content="Crackteck">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="description" content="Technofra crackteck, Multipurpose eCommerce ">
     <meta name="csrf-token" content="{{ csrf_token() }}">

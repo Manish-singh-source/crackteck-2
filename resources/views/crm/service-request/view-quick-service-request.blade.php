1182c55d9ca9d291a9dbb9e94fcdf817a679f836
saurabh <support@technofra.com>
2026-03-19 12:12:58 +0530

    fixed

--- resources/views/crm/service-request/view-quick-service-request.blade.php
+++ resources/views/crm/service-request/view-quick-service-request.blade.php
@@ -1092,7 +1092,7 @@ $isProcessed = in_array($currentStatus, [
                                 <h5 class="card-title mb-0">Assign Engineer</h5>
                             </div>
                             <div class="card-body">
-                                <form id="assignEngineerForm">
+                                <form id="assignRemoteEngineerForm">
                                     @csrf
                                     <input type="hidden" name="service_request_id" value="{{ $request->id }}">
                                     <input type="hidden" name="service_type"
